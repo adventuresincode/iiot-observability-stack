6b8c82d58c99335acaa6ebd3817101f24cb00bb0
kjayantmenon <kjayantmenon@gmail.com>
2022-08-23 14:45:14 +0000

    Added the code to scrape from a public OPCUA endpoint

---
+++ src/go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.18
+
+require (
+	github.com/gopcua/opcua v0.3.5 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,7 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+
+	"github.com/gopcua/opcua"
+	"github.com/gopcua/opcua/id"
+	"github.com/gopcua/opcua/monitor"
+	"github.com/gopcua/opcua/ua"
 )
 
 func main() {
@@ -9,6 +20,8 @@ func main() {
 	fmt.Println("Reading from public OPCUA end points")
 	opcua_endpoints := getPublicOPCUAEndpoints()
 	printEndPoints(opcua_endpoints)
+	browseEndPoint(opcua_endpoints[0])
+	monitorDevices(opcua_endpoints[0])
 }
 
 func getPublicOPCUAEndpoints() []string {
@@ -27,6 +40,133 @@ func getPublicOPCUAEndpoints() []string {
 func printEndPoints(opcua_endpoints []string) {
 
 	for index, en := range opcua_endpoints {
-		fmt.Println(" ", index, ". ", en)
+		fmt.Println(" ", index+1, ". ", en)
 	}
+}
+
+func browseEndPoint(endpoint string) {
+
+	nodeID := "ns=0;i=85"
+
+	ctx := context.Background()
+
+	// Connect
+	c := opcua.NewClient(endpoint)
+	if err := c.Connect(ctx); err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	nid, err := ua.ParseNodeID(nodeID)
+	if err != nil {
+		log.Fatalf("invalid node id: %s", nodeID)
+	}
+
+	n := c.Node(nid)
+
+	// attrs, err := n.Attributes(ua.AttributeIDBrowseName, ua.AttributeIDDataType)
+	// if err != nil {
+	// 	log.Fatalf("invalid attribute: %s", err.Error())
+	// }
+	//fmt.Printf("BrowseName: %s; DataType: %s\n", attrs[0].Value, getDataType(attrs[1]))
+	//fmt.Printf("BrowseName: %s; DataType: %s\n", attrs[1].Value, getDataType(attrs[1]))
+
+	// Get children
+	refs, err := n.ReferencedNodes(id.HasComponent, ua.BrowseDirectionForward, ua.NodeClassAll, true)
+	if err != nil {
+		log.Fatalf("References: %s", err)
+	}
+
+	fmt.Printf("Children: %d\n", len(refs))
+	for _, rn := range refs {
+		fmt.Printf("   %s\n", rn.ID.String())
+	}
+}
+
+func getDataType(value *ua.DataValue) string {
+	if value.Status != ua.StatusOK {
+		return value.Status.Error()
+	}
+
+	switch value.Value.NodeID().IntID() {
+	case id.DateTime:
+		return "time.Time"
+
+	case id.Boolean:
+		return "bool"
+
+	case id.Int32:
+		return "int32"
+	}
+
+	return value.Value.NodeID().String()
+}
+
+func monitorDevices(endpoint string) {
+	nodeID := "ns=2;s=Dynamic/RandomFloat"
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		<-signalCh
+		println()
+		cancel()
+	}()
+
+	c := opcua.NewClient(endpoint, opcua.SecurityMode(ua.MessageSecurityModeNone))
+
+	if err := c.Connect(ctx); err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	m, err := monitor.NewNodeMonitor(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m.SetErrorHandler(func(_ *opcua.Client, sub *monitor.Subscription, err error) {
+		log.Printf("error: sub=%d err=%s", sub.SubscriptionID(), err.Error())
+	})
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+	go startCallbackSub(ctx, m, time.Second, 0, wg, nodeID)
+	<-ctx.Done()
+	wg.Wait()
+}
+
+func cleanup(ctx context.Context, sub *monitor.Subscription, wg *sync.WaitGroup) {
+	log.Printf("stats: sub=%d delivered=%d dropped=%d", sub.SubscriptionID(), sub.Delivered(), sub.Dropped())
+	sub.Unsubscribe(ctx)
+	wg.Done()
+}
+
+func startCallbackSub(ctx context.Context, m *monitor.NodeMonitor, interval, lag time.Duration, wg *sync.WaitGroup, nodes ...string) {
+	sub, err := m.Subscribe(
+		ctx,
+		&opcua.SubscriptionParameters{
+			Interval: interval,
+		},
+		func(s *monitor.Subscription, msg *monitor.DataChangeMessage) {
+			if msg.Error != nil {
+				log.Printf("[callback] error=%s", msg.Error)
+			} else {
+				log.Printf("[callback] node=%s value=%v", msg.NodeID, msg.Value.Value())
+			}
+			time.Sleep(lag)
+		},
+		nodes...)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer cleanup(ctx, sub, wg)
+
+	<-ctx.Done()
 }
